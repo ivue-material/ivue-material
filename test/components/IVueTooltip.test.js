c1faa5aef4aaf1b798b45706f0858523254e8756
qq282126990 <282126990@qq.com>
2019-07-10 13:59:53 +0800

    ivue tooltip test

--- test/components/IVueTooltip.test.js
+++ test/components/IVueTooltip.test.js
@@ -1,19 +1,25 @@
 import IvueTooltip from '../../src/components/ivue-tooltip';
 import { mount } from '@vue/test-utils';
 
-describe('IvueListItem', function () {
-      it('should render the switch theme', async () => {
-            // const wrapper = mount({
-            //       template: `
-            //             <IvueSwitch class="red-accent-4" :value="true" ></IvueSwitch>
-            //       `,
-            //       components: {
-            //             IvueSwitch
-            //       }
-            // }, {
-            //       });
+describe('IvueTooltip', function () {
+    it('should render the display arrow', async () => {
+        const wrapper = mount({
+            template: `
+            <IvueTooltip
+                color="#303133"
+                content="Here is the prompt text"
+                arrow
+            >A balloon appears when the mouse passes over this text</IvueTooltip>
+            `,
+            components: {
+                IvueTooltip
+            }
+        }, {
+                attachToDocument: true
+            });
 
-            // expect(wrapper.classes()).to.contain('red-accent-4');
-      });
+
+          expect(wrapper.find('.ivue-tooltip-popper--arrow').element.nodeName).to.be.equal('DIV');
+    });
 
 });
